4e2206a1b6c7e2963a1e2aa2e697032706111962
hendisantika <hendisantika@yahoo.co.id>
2026-01-02 18:01:08 +0700

    feat: init project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/indonesiamap/IndonesiaMapApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.indonesiamap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndonesiaMapApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(IndonesiaMapApplication.class, args);
+    }
+
+}
